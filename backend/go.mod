6758229d950f818dda25e756d8949c81f943525e
Alexx Bull <alexxbull@outlook.com>
2020-04-30 13:23:25 -0700

    added protobuf for chat added go module for dependencies

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/alexxbull/rpchat/backend
+
+go 1.14
---
+++ backend/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	addr    = "localhost:8080"
+	network = "tcp"
+)
+
+func main() {
+	var opts []grpc.ServerOption
+
+	// tls security
+
+	server := grpc.NewServer(opts...)
+
+	lis, err := net.Listen(network, addr)
+	if err != nil {
+		log.Fatalf("Unable to listen on %v: %v", addr, err)
+	}
+
+	// Register services
+
+	server.Serve(lis)
+}
+
+func sendMessages() {
+
+	// client := chat.NewChatServiceClient(conn)
+}
