e779d42534781687a4dba2626a6666fa3a842399
Alexx Bull <alexxbull@outlook.com>
2020-05-06 23:39:51 -0700

    added connection to local postgres database

--- backend/go.mod
+++ backend/go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.3.3
+	github.com/lib/pq v1.5.1
 	google.golang.org/grpc v1.29.1
 )
--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/golang/protobuf/ptypes"
-
 	"github.com/golang/protobuf/ptypes/timestamp"
-
+	"github.com/lib/pq"
 	"google.golang.org/grpc"
 
 	chat "github.com/alexxbull/rpchat/backend/pb"
 )
 
 const (
-	addr    = "localhost:8080"
-	network = "tcp"
+	addr       = "localhost:8080"
+	network    = "tcp"
+	dbConnInfo = "dbname=grpchat user=alexx password=admin sslmode=disable"
 )
 
 func main() {
+	// connect to database
+	db, err := connectDatabase()
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer db.Close()
+
+	// run postgres listener for deleted records in images table
+	go dbListener()
+
+	// create grpc server
 	var opts []grpc.ServerOption
 
 	// TODO: add tls security
@@ -36,8 +50,66 @@ func main() {
 	// register services
 	chat.RegisterChatServiceServer(server, &chatServer{})
 
-	// start server
+	// start grpc server
+	fmt.Println("Starting server...")
 	server.Serve(lis)
+}
+
+func connectDatabase() (*sql.DB, error) {
+	db, err := sql.Open("postgres", dbConnInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to database")
+
+	return db, nil
+}
+
+func dbListener() {
+	reportConnError := func(event pq.ListenerEventType, err error) {
+		if err != nil {
+			fmt.Println("Connection error while listening to database:", err)
+		}
+	}
+
+	listener := pq.NewListener(dbConnInfo, 10*time.Second, time.Minute, reportConnError)
+	err := listener.Listen("events")
+	if err != nil {
+		log.Fatalln("Unable to listen to pg Listener:", err)
+	}
+
+	// listen for image deletions
+	fmt.Println("Listening to database...")
+	for {
+		notice := <-listener.Notify
+		payload := notice.Extra
+
+		//Unmarshal payload
+		type deleteImage struct {
+			Path string `json:"path"`
+		}
+
+		var imgDelete deleteImage
+		err = json.Unmarshal([]byte(payload), &imgDelete)
+		if err != nil {
+			log.Fatalln("Unmarsharlling payload error:", err)
+		}
+
+		// delete file
+		err = os.Remove(imgDelete.Path)
+		if err != nil {
+			fmt.Println("Unable to delete file:", err)
+			continue
+		}
+
+		fmt.Println("File deleted:", imgDelete.Path)
+	}
 }
 
 type chatServer struct{}
