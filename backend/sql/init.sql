2970e5b9dcb59cc4694f71f99076255e22e3dda0
Alexx Bull <alexxbull@outlook.com>
2020-05-10 12:55:04 -0700

    added NOT NULL condition to UNIQUE fields added constraint to not allow message to contain only whitespaces or no characters

--- backend/sql/init.sql
+++ backend/sql/init.sql
@@ -1,41 +1,43 @@
+-- Tables
 CREATE TABLE channels (
 	id SERIAL PRIMARY KEY,
-	channel_name TEXT UNIQUE,
-	creation_date TIMESTAMPTZ DEFAULT NOW(),
-	description TEXT
+	channel_name TEXT UNIQUE NOT NULL,
+	creation_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	description TEXT,
+	channel_owner TEXT DEFAULT 'admin' REFERENCES users(user_name) ON UPDATE CASCADE ON DELETE SET DEFAULT 
 );
 
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,	
-	email TEXT UNIQUE,
-	image_path TEXT UNIQUE,
-	join_date TIMESTAMPTZ DEFAULT NOW(),
-	user_name TEXT UNIQUE,
-	user_password TEXT
+	email TEXT UNIQUE NOT NULL,
+	image_path TEXT UNIQUE NOT NULL,
+	join_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+	user_name TEXT UNIQUE NOT NULL,
+	user_password TEXT NOT NULL
 );
 
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
-	user_name TEXT REFERENCES users(user_name) ON UPDATE CASCADE ON DELETE CASCADE,
+	user_name TEXT REFERENCES users(user_name) ON UPDATE CASCADE,
 	channel_name TEXT REFERENCES channels(channel_name) ON UPDATE CASCADE ON DELETE CASCADE,
-	message TEXT,
-	post_date TIMESTAMPTZ NOT NULL DEFAULT NOW()
+	message TEXT NOT NULL,
+	post_date TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE images (
 	id SERIAL PRIMARY KEY,
 	message_id INTEGER REFERENCES messages(id),
 	height INTEGER,
-	path TEXT UNIQUE,
+	path TEXT UNIQUE NOT NULL,
 	size INTEGER,
 	width INTEGER
 );
 
-
+-- Indexes
 CREATE INDEX idx_message_user_name ON messages(user_name);
-
 CREATE INDEX idx_message_channel_name ON messages(channel_name);
 
+--Triggers
 CREATE OR REPLACE FUNCTION delete_server_image() RETURNS TRIGGER AS $$
 DECLARE 
     payload jsonb; 
@@ -52,3 +54,4 @@ CREATE TRIGGER image_deleted
 AFTER DELETE ON images
 FOR EACH ROW
 EXECUTE PROCEDURE delete_server_image();
+
