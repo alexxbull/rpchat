2d76d529b9bda9dda7c28b2518fab39a53e4ac82
Alexx Bull <alexxbull@outlook.com>
2020-05-17 14:41:50 -0700

    added Chat component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Backdrop from './components/Backdrop/Backdrop.js'
 import Toolbar from './containers/Toolbar/Toolbar.js'
+import Chat from './containers/Chat/Chat.js';
 
 function App() {
   return (
     <div className="App">
       <Backdrop />
       <Toolbar />
+      <Chat />
     </div>
   );
 }
---
+++ frontend/src/containers/Chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Chat = props => {
+    return (
+        <div className="chat">
+            <div>Channels Component</div>
+            <div>Messages components</div>
+            <div>Users Component</div>
+        </div>
+    )
+}
+export default Chat;
