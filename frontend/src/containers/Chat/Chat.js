7cb06c428cc31ac9ab0b16c93e2cfb1caf03aa03
Alexx Bull <alexxbull@outlook.com>
2020-05-17 15:17:09 -0700

    added css styling for page layout

--- frontend/src/containers/Chat/Chat.js
+++ frontend/src/containers/Chat/Chat.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 import Channels from '../../components/Channels/Channels';
 
+import classes from './Chat.module.css'
+
 const Chat = props => {
     return (
-        <div className="chat">
+        <div className={classes.Chat}>
             <Channels />
             <div>Messages components</div>
             <div>Users Component</div>
