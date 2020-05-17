3b017d29630b8101d98bc3d2b279e9a72332d5db
Alexx Bull <alexxbull@outlook.com>
2020-05-16 20:55:26 -0700

    added Backdrop component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+
+import Backdrop from './components/Backdrop/Backdrop.js'
+
 
 function App() {
   return (
     <div className="App">
-      RPChat
+      <Backdrop />
     </div>
   );
 }
