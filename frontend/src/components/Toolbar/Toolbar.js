8b6d360efb6404ec2971a34d62828f7f700726c4
Alexx Bull <alexxbull@outlook.com>
2020-05-16 21:37:47 -0700

    added toggling sidebar on button click

--- frontend/src/components/Toolbar/Toolbar.js
+++ frontend/src/components/Toolbar/Toolbar.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import Sidebar from '../Sidebar/Sidebar.js'
 
 const Toolbar = props => {
+    const [showSidebar, setShowSideBar] = useState(false)
+
+    const sidebar = showSidebar ? <Sidebar /> : null
+
     return (
         <div className="toolbar">
-            <button className="sidebar-btn">Sidebar</button>
-            <Sidebar />
+            <button
+                className="sidebar-btn"
+                onClick={setShowSideBar.bind(this, !showSidebar)}
+            >Sidebar</button>
+            {sidebar}
             <h1>Channel name</h1>
             <button className="settings-btn">Settings</button>
         </div>
