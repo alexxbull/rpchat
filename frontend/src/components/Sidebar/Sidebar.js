bb899a273937572b910486474b31a536014712eb
Alexx Bull <alexxbull@outlook.com>
2020-05-16 21:10:47 -0700

    added Sidebar component

---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sidebar = props => {
+    return (
+        <div className="sidebar">
+            <button className="channels-btn">Channels Menu</button>
+            <button className="users-btn">Users Menu</button>
+        </div>
+    )
+}
+
+export default Sidebar
--- frontend/src/components/Toolbar/Toolbar.js
+++ frontend/src/components/Toolbar/Toolbar.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Sidebar from '../Sidebar/Sidebar.js'
+
 const Toolbar = props => {
     return (
         <div className="toolbar">
             <button className="sidebar-btn">Sidebar</button>
+            <Sidebar />
             <h1>Channel name</h1>
             <button className="settings-btn">Settings</button>
         </div>
