674cfc499c505f06566b2d6408f619d7080bea40
Alexx Bull <alexxbull@outlook.com>
2020-05-21 03:46:21 -0700

    added styling for Backdrop component and conditional rendering

--- frontend/src/components/Backdrop/Backdrop.js
+++ frontend/src/components/Backdrop/Backdrop.js
@@ -2,6 +2,20 @@ import React from 'react'
 
 import classes from './Backdrop.module.css'
 
-const Backdrop = props => <div className={classes.Backdrop}></div>
+
+const Backdrop = props => {
+    let backdrop = null
+
+    if (props.show) {
+        backdrop =
+            <div
+                className={classes.Backdrop}
+                onClick={props.click}
+            >
+            </div>
+    }
+
+    return backdrop
+}
 
 export default Backdrop
