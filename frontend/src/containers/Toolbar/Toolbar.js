478fc73f43c82f429dcb062f8a15bbb7aaa10872
Alexx Bull <alexxbull@outlook.com>
2020-05-21 03:49:44 -0700

    added Backdrop component to Toolbar component removed Sidebar component added Channels component to Toolbar component added condition rendering of Backdrop component when clicking Channels button

--- frontend/src/containers/Toolbar/Toolbar.js
+++ frontend/src/containers/Toolbar/Toolbar.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react'
 
-import Sidebar from '../../components/Sidebar/Sidebar.js'
 import Settings from '../Settings/Settings.js'
+import Channels from '../../components/Channels/Channels.js'
+import Backdrop from '../../components/Backdrop/Backdrop.js'
 
 const Toolbar = props => {
-    const [showSidebar, setShowSideBar] = useState(false)
+    const [showChannels, setshowChannels] = useState(false)
 
-    const sidebar = showSidebar ? <Sidebar /> : null
+    const handleBackdrop = () => {
+        setshowChannels(false)
+    }
 
     return (
         <div className="toolbar">
+            <Backdrop show={showChannels} click={handleBackdrop} />
             <button
-                className="sidebar-btn"
-                onClick={setShowSideBar.bind(this, !showSidebar)}
-            >Sidebar</button>
-            {sidebar}
+                className="channels-btn"
+                onClick={setshowChannels.bind(this, !showChannels)}
+            >Channels</button>
+            <Channels show={showChannels} />
             <h1>Channel name</h1>
             <Settings />
         </div>
