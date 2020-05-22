dedd247a26d3853b90cb35114c9e7466f4836dbd
Alexx Bull <alexxbull@outlook.com>
2020-05-21 17:24:22 -0700

    added conditional rendering of Toolbar component depending on desktop or mobile view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Backdrop from './components/Backdrop/Backdrop.js'
 import Toolbar from './containers/Toolbar/Toolbar.js'
 import Chat from './containers/Chat/Chat.js';
 
+const rem = 16
+
 function App() {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 40 * rem) // +641px = desktop
+  const updateView = () => setDesktop(window.innerWidth > 40 * rem)
+
+  useEffect(() => {
+    window.addEventListener('resize', updateView)
+    return () => window.removeEventListener('resize', updateView)
+  })
+
+  const toolbar = isDesktop ? null : <Toolbar />
+
   return (
     <div className="App">
       <Backdrop />
-      <Toolbar />
+      {toolbar}
       <Chat />
     </div>
   );
