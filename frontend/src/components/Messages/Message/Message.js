83776a67e68a14c9d06d5be00431034333898881
Alexx Bull <alexxbull@outlook.com>
2020-05-24 13:50:10 -0700

    changed rendered message data

--- frontend/src/components/Messages/Message/Message.js
+++ frontend/src/components/Messages/Message/Message.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+import classes from '../Messages.module.css';
+
 const Message = props =>
-    <div className="message">
-        <div className="message-user">{props.user}</div>
-        <p>{props.memo}</p>
-        <div className="message-time">{props.timestamp}</div>
-    </div>
+    < div className={classes.Message} >
+        <img className={classes.User_icon} src={props.avatar} alt={`${props.user} avatar`} />
+        <div className={classes.Message_content}>
+            <span className={classes.User_name}>{props.username}
+                <span className={classes.Message_date}>{'date ' + props.timestamp}</span>
+                <span className={classes.Message_date}>{'time ' + props.timestamp}</span>
+            </span>
+            <ol className={classes.Message_text_list}>
+                <li className={classes.Message_text_item}>
+                    <p className={classes.Message_text}>{props.memo}</p>
+                    <div className={classes.Message_time}>{props.timestamp}</div>
+                </li>
+            </ol>
+        </div>
+    </div >
 
 export default Message;
