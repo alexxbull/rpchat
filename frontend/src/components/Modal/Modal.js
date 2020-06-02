d44d1dac5861c4dbbdf86ed4d1a3c73989ad0682
Alexx Bull <alexxbull@outlook.com>
2020-06-01 22:40:17 -0700

    render Modal component out of view by default

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -5,13 +5,21 @@ import Backdrop from '../Backdrop/Backdrop'
 
 
 const Modal = props => {
+    const modalClasses = [classes.Modal]
+    let backdrop = null
+
+    if (props.show) {
+        modalClasses.push(classes.Open)
+        backdrop = <Backdrop click={props.exit} show={props.show && props.isDesktop} />
+    }
+
     return (
         <>
-            <Backdrop click={props.exit} show={true} />
-            <div className={classes.Modal}>
+            {backdrop}
+            <div className={modalClasses.join(' ')}>
                 <header>
                     <h1 className={classes.Title}>{props.title}</h1>
-                    <div className={classes.Close}><button className={classes.CloseBtn} onClick={props.exit}></button></div>
+                    <div className={classes.CloseBtnContainer}><button className={classes.CloseBtn} onClick={props.exit}></button></div>
                 </header>
                 <div>{props.children}</div>
                 <button className={classes.OkayBtn} onClick={props.click}>{props.submitText}</button>
