c019009ff3adf99d43d840e333633d677b8ab93d
Alexx Bull <alexxbull@outlook.com>
2020-06-01 22:33:36 -0700

    moved Modal rendering from ChannelsHeader to Channels component render channels out of view by default

--- frontend/src/components/Channels/Channels.js
+++ frontend/src/components/Channels/Channels.js
@@ -5,9 +5,10 @@ import classes from './Channels.module.css'
 import Channel from './Channel/Channel.js'
 import ChannelsHeader from './ChannelsHeader/ChannelsHeader.js';
 import SettingsBar from '../SettingsBar/SettingsBar';
+import Modal from '../Modal/Modal.js';
 
 const Channels = props => {
-    const [modal, setModal] = useState(null)
+    const [showModal, setShowModal] = useState(false)
 
     const channelsData = [
         { name: "Channel1", id: 1 },
@@ -15,30 +16,42 @@ const Channels = props => {
         { name: "Channel3", id: 3 },
     ]
 
-    let attachedClasses = [classes.Channels, classes.Close]
+    const channels = channelsData.map(channel =>
+        <Channel
+            key={channel.id}
+            name={channel.name}
+        />
+    )
 
-    let channels = null
+    const channelsClasses = [classes.Channels]
     if (props.show) {
-        channels = channelsData.map(channel =>
-            <Channel
-                key={channel.id}
-                name={channel.name}
-            />
-        )
-
-        attachedClasses = [classes.Channels, classes.Open]
+        channelsClasses.push(classes.Open)
     }
 
     return (
         <>
-            <div className={attachedClasses.join(' ')}>
-                <ChannelsHeader showModal={setModal} />
+            <div className={channelsClasses.join(' ')}>
+                <ChannelsHeader displayModal={setShowModal.bind(this, true)} />
                 <ul className={classes.Channels__list}>
                     {channels}
                 </ul>
                 <SettingsBar />
             </div>
-            {modal}
+            <Modal
+                show={showModal}
+                title={"Create Channel"}
+                exit={setShowModal.bind(this, false)}
+                submitText={"Submit"}
+                isDesktop={props.isDesktop}
+            >
+                <form className={classes.AddChannelForm}>
+                    <label htmlFor="Channel Name">Name:</label>
+                    <input type="text" name={"ChannelName"} placeholder={"Channel name"} />
+
+                    <label htmlFor="Channel Description">Description:</label>
+                    <input type="text" name={"Channel Description"} placeholder={"Describe the topics of this channel"} />
+                </form>
+            </Modal>
         </>
     )
 }
--- frontend/src/components/Channels/ChannelsHeader/ChannelsHeader.js
+++ frontend/src/components/Channels/ChannelsHeader/ChannelsHeader.js
@@ -2,32 +2,12 @@ import React from 'react'
 
 import classes from './ChannelsHeader.module.css'
 
-import Modal from '../../Modal/Modal.js'
-
 const ChannelsHeader = props => {
-    const handleModalClose = () => {
-        props.showModal(null)
-    }
-
-    const handleAddChannel = () => {
-        props.showModal(
-            <Modal title={"Create Channel"} exit={handleModalClose} submitText={"Submit"}>
-                <form className={classes.AddChannelForm}>
-                    <label htmlFor="Channel Name">Name:</label>
-                    <input type="text" name={"ChannelName"} placeholder={"Channel name"} />
-
-                    <label htmlFor="Channel Description">Description:</label>
-                    <input type="text" name={"Channel Description"} placeholder={"Describe the topics of this channel"} />
-                </form>
-            </Modal>
-        )
-    }
-
     return (
         <header className={classes.ChannelsHeader}>
             <div className={classes.Channels_icon}></div>
             <h1 className={classes.ChannelsHeader_title}>Channels</h1>
-            <button className={classes.Add_btn} onClick={handleAddChannel}>+</button>
+            <button className={classes.Add_btn} onClick={props.displayModal}>+</button>
         </header>
     )
 }
