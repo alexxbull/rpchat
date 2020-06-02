83090b15cf0f841ca912383d9cda24af7627fd0e
Alexx Bull <alexxbull@outlook.com>
2020-06-01 22:39:13 -0700

    moved rendering and handling of Channels and Users components to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,20 @@ import Toolbar from './containers/Toolbar/Toolbar.js'
 import Chat from './containers/Chat/Chat.js';
 import Channels from './components/Channels/Channels.js';
 import Users from './components/Users/Users.js';
+import Backdrop from './components/Backdrop/Backdrop.js';
 
 const rem = 16
 
 function App() {
   const [isDesktop, setDesktop] = useState(window.innerWidth > 40 * rem) // +641px = desktop
   const updateView = () => setDesktop(window.innerWidth > 40 * rem)
+
+  const [showChannels, setShowChannels] = useState(false)
+  const [showUsers, setShowUsers] = useState(false)
+
+  const toggleBackdrop = () => {
+    showChannels ? setShowChannels(false) : setShowUsers(false)
+  }
 
   useEffect(() => {
     window.addEventListener('resize', updateView)
@@ -18,10 +26,16 @@ function App() {
 
   return (
     <div className="App">
-      <Toolbar show={!isDesktop} />
-      <Channels show={isDesktop} />
+      <Backdrop show={!isDesktop && (showChannels || showUsers)} click={toggleBackdrop} isDesktop={isDesktop} />
+      <Toolbar
+        show={!isDesktop}
+        isDesktop={isDesktop}
+        showChannels={setShowChannels.bind(this, true)}
+        showUsers={setShowUsers.bind(this, true)}
+      />
+      <Channels show={isDesktop || showChannels} isDesktop={isDesktop} />
       <Chat />
-      <Users show={isDesktop} />
+      <Users show={isDesktop || showUsers} />
     </div>
   );
 }
--- frontend/src/containers/Toolbar/Toolbar.js
+++ frontend/src/containers/Toolbar/Toolbar.js
@@ -1,43 +1,26 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import classes from './Toolbar.module.css'
 
-import BackdropContext from '../../context/BackdropContext.js'
-
-import Channels from '../../components/Channels/Channels.js'
-import Backdrop from '../../components/Backdrop/Backdrop.js'
-import Users from '../../components/Users/Users.js'
-
 const Toolbar = props => {
-    const [showChannels, setShowChannels] = useState(false)
-    const [showUsers, setShowUsers] = useState(false)
-
-    const handleBackdrop = () => {
-        showChannels ? setShowChannels(false) : setShowUsers(false)
-    }
-
     let toolbar = null
+
     if (props.show) {
-        toolbar =
-            <BackdropContext.Provider value={showChannels}>
-                <Backdrop show={showChannels || showUsers} click={handleBackdrop} />
-                <div className={classes.Toolbar}>
-                    <button
-                        className={classes.Channels_btn}
-                        onClick={setShowChannels.bind(this, !showChannels)}
-                    >
-                        <span className={classes.Channels_btn_bar}></span>
-                        <span className={classes.Channels_btn_bar}></span>
-                        <span className={classes.Channels_btn_bar}></span>
-                    </button>
-                    <Channels show={showChannels} />
+        const channelsButton =
+            <button
+                className={classes.Channels_btn}
+                onClick={props.showChannels}
+            >
+                <span className={classes.Channels_btn_bar}></span>
+                <span className={classes.Channels_btn_bar}></span>
+                <span className={classes.Channels_btn_bar}></span>
+            </button>
 
-                    <h1 className={classes.Channel_name}>Current Channel</h1>
-
-                    <button className={classes.Users_btn} onClick={setShowUsers.bind(this, !showUsers)}></button>
-                    <Users show={showUsers} />
-                </div>
-            </BackdropContext.Provider>
+        toolbar = <div className={classes.Toolbar}>
+            {channelsButton}
+            <h1 className={classes.Channel_name}>Current Channel</h1>
+            <button className={classes.Users_btn} onClick={props.showUsers}></button>
+        </div>
     }
 
     return toolbar
