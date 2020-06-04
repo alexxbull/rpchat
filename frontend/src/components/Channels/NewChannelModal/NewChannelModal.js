fd82998b2c0adaa9016e7e12999aa953be58c697
Alexx Bull <alexxbull@outlook.com>
2020-06-03 22:03:56 -0700

    added posting of new channel to server/database

--- frontend/src/components/Channels/NewChannelModal/NewChannelModal.js
+++ frontend/src/components/Channels/NewChannelModal/NewChannelModal.js
@@ -1,34 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import classes from './NewChannelModal.module.css'
 
-import Modal from '../../Modal/Modal'
+import Modal from '../../Modal/Modal.js'
+import { NewChannelRequest } from '../../../proto/chat_pb.js'
+import { ChatServiceClient } from '../../../proto/chat_grpc_web_pb.js'
+
+const hostname = 'http://localhost:8080'
 
 const NewChannelModal = props => {
-    const submitBtn = <input className={classes.SubmitBtn} type="submit" onSubmit={props.click} value={props.submitText} form={classes.NewChannelModal} />
+    const blankChannel = {
+        name: '',
+        desc: '',
+        owner: 'TestUser',
+        error: ''
+    }
+    const [newChannel, setNewChannel] = useState(blankChannel)
+
+    const inputChangeHandler = event => {
+        const eventName = event.target.name
+        setNewChannel({ ...newChannel, [eventName]: event.target.value, error: '' })
+    }
+
+    const handleModalClose = () => {
+        setNewChannel(blankChannel)
+        props.close()
+    }
 
     const handleSubmit = event => {
         event.preventDefault()
+        console.log('attempting to add new channel')
 
-        console.log('form submitted')
-        props.close()
+        const client = new ChatServiceClient(hostname)
+        const req = new NewChannelRequest()
+        req.setName(newChannel.name)
+        req.setDescription(newChannel.desc)
+        req.setOwner(newChannel.owner)
+
+        client.addChannel(req, {}, (err, res) => {
+            if (err) {
+                console.log(err)
+                setNewChannel({ ...newChannel, error: err.message })
+            }
+            if (res) {
+                console.log('form submitted')
+                handleModalClose()
+            }
+        })
     }
 
     return (
         <Modal
+            error={newChannel.error}
             show={props.show}
             title={"Create Channel"}
-            close={props.close}
+            close={handleModalClose}
             isDesktop={props.isDesktop}
             formID={classes.NewChannelModal}
-            okayBtn={submitBtn}
+            okayBtn={<input className={classes.SubmitBtn} type="submit" onSubmit={props.click} value={props.submitText} form={classes.NewChannelModal} />}
         >
             <form id={classes.NewChannelModal} onSubmit={handleSubmit}>
                 <label htmlFor="Channel Name">Name:</label>
-                <input required type="text" name={"ChannelName"} placeholder={"Channel name"} pattern={"^(?!\\s*$).+"} title={"Channel name cannot only be whitespace"} />
+                <input required type="text" placeholder={"Channel name"} name={"name"} value={newChannel.name} onChange={inputChangeHandler} pattern={"^(?!\\s*$).+"} title={"Channel name cannot only be whitespace"} />
 
                 <label htmlFor="Channel Description">Description:</label>
-                <input type="text" name={"Channel Description"} placeholder={"Describe the topics of this channel"} />
+                <input type="text" placeholder={"Describe the topics of this channel"} name={"desc"} value={newChannel.desc} onChange={inputChangeHandler} />
             </form>
         </Modal>
     )
