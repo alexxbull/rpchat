a6dcb618242e90d0c55842a41575c10e5c9d70ee
Alexx Bull <alexxbull@outlook.com>
2020-06-07 18:50:46 -0700

    added Login page

---
+++ frontend/src/containers/Login/Login.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import classes from './Login.module.css'
+
+
+const Login = props => {
+    const [error, setError] = useState('')
+    const [username, setUsername] = useState({ value: '', styles: [classes.Username] })
+    const [password, setPassword] = useState({ value: '', styles: [classes.Password] })
+
+    const handleInput = event => {
+        switch (event.target.name) {
+            case "username":
+                setUsername({ value: event.target.value, styles: [classes.Username] })
+                break
+            case "password":
+                setPassword({ value: event.target.value, styles: [classes.Password] })
+                break
+            default:
+                break
+        }
+        if (error)
+            setError('')
+    }
+
+    const handleLogin = event => {
+        event.preventDefault()
+
+        if (!username.value)
+            setUsername({ ...username, styles: [classes.Username, classes.InputError] })
+
+        if (!password.value)
+            setPassword({ ...password, styles: [classes.Password, classes.InputError] })
+
+        if (username.value && password.value) {
+            console.log('login')
+        }
+        else {
+            setError('Invalid login')
+        }
+    }
+
+    const errorClasses = [classes.Error]
+    if (error)
+        errorClasses.push(classes.ShowError)
+
+    return (
+        <div className={classes.Container}>
+            <div className={classes.Login}>
+                <div className={errorClasses.join(' ')}>{error}</div>
+                <h1>RPChat</h1>
+                <form className={classes.Login_Form} onSubmit={handleLogin}>
+                    <label htmlFor="Username">Username</label>
+                    <input className={username.styles.join(' ')} type="text" placeholder="Enter username" name="username" value={username.value} onChange={handleInput} />
+
+                    <label htmlFor="password">Password</label>
+                    <input className={password.styles.join(' ')} type="password" placeholder="Enter password" name="password" value={password.value} onChange={handleInput} />
+
+                    <input className={classes.LoginBtn} type="submit" value="LOG IN" />
+                </form>
+                <div className={classes.RegisterLink}><Link to="/signup">Create an account</Link></div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
