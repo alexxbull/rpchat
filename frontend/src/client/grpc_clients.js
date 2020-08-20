0b8d8ae7fa018811d137a7850e772f66d567df1c
Alexx Bull <alexxbull@outlook.com>
2020-08-20 13:58:33 -0700

    read grpc server address from environment variable

--- frontend/src/client/grpc_clients.js
+++ frontend/src/client/grpc_clients.js
@@ -3,7 +3,8 @@ import { AuthServicePromiseClient } from '../proto/auth/auth_grpc_web_pb.js'
 import { AuthUnaryInterceptor } from '../interceptors/AuthUnaryInterceptor.js'
 import { AuthStreamInterceptor } from '../interceptors/AuthStreamInterceptor.js'
 
-const hostname = 'https://localhost:443'
+const hostname = process.env.REACT_APP_GRPC_SERVER_HOST || 'https://localhost:443'
+console.log('hostname', hostname);
 
 const AuthClient = dispatch => {
     const authUnaryInterceptor = new AuthUnaryInterceptor(dispatch)
